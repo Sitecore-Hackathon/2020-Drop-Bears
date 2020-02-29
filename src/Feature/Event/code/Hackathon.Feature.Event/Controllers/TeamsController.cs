f868808bb812dc681d5d0316234c3be03eeacdea
Mattie <matthew.ellins@gmail.com>
2020-02-29 21:19:10 +0800

    Renamed controller to what it should be, renamed asm name

---
+++ src/Feature/Event/code/Hackathon.Feature.Event/Controllers/TeamsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+
+namespace Hackathon.Feature.Event.Controllers
+{
+    public class TeamsController : SitecoreController
+    {
+        // GET: Lists
+        public ActionResult List(string itemId)
+        {
+//            var render = new Sitecore.Mvc.Presentation.RenderingModel();
+//            var item = Sitecore.Mvc.Presentation.RenderingContext.Current.Rendering.Item;
+
+            var web = Sitecore.Configuration.Factory.GetDatabase("web");
+            var teams = web.GetItem(itemId);
+            var teamList = teams.GetChildren();           
+
+            return View();
+        }
+    }
+}
