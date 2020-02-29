5f0ab07120972db49cfb195f8ee5a7d48647f6d9
Mattie <matthew.ellins@gmail.com>
2020-02-29 10:35:37 +0800

    Initial commit

---
+++ src/Feature/Navigation/code/Hackathon.Feature.Navigation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HBFWeb.Feature.Navigation")]
+[assembly: AssemblyDescription("HBFWeb.Feature.Navigation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HBF Health Ltd")]
+[assembly: AssemblyProduct("HBFWeb.Feature.Navigation")]
+[assembly: AssemblyCopyright("Copyright © HBF Health Ltd 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2724779-e2fd-41f3-8b0b-9bb756615ccf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
